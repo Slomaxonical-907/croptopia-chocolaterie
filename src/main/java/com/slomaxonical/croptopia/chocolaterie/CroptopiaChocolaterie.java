2b30d3224f873ddcf4e389566eaad8cf343a2f74
Slomaxis <Slomaxonical907@gmail.com>
2021-12-09 21:44:57 +0200

    virtual not working

--- src/main/java/com/slomaxonical/croptopia/chocolaterie/CroptopiaChocolaterie.java
+++ src/main/java/com/slomaxonical/croptopia/chocolaterie/CroptopiaChocolaterie.java
@@ -3,6 +3,9 @@ package com.slomaxonical.croptopia.chocolaterie;
 import com.slomaxonical.croptopia.chocolaterie.registry.ItemRegistry;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 
@@ -16,8 +19,13 @@ public class CroptopiaChocolaterie implements ModInitializer {
     public static Item.Settings createGroup() {
             return new FabricItemSettings().group(CROPTOPIA_ITEM_GROUP);
         }
+    static final String CROPTOPIA_MOD_ID = "croptopia";
+
     @Override
     public void onInitialize() {
+        FabricLoader.getInstance().getModContainer(CROPTOPIA_MOD_ID)
+                .map(modContainer -> ResourceManagerHelper.registerBuiltinResourcePack(new Identifier("cacao","croptop"),  modContainer, ResourcePackActivationType.DEFAULT_ENABLED))
+                .filter(success -> !success);
         ItemRegistry.init();
     }
 }
