96326a24364420adc195489be6f508b557a060cb
Slomaxis <Slomaxonical907@gmail.com>
2021-12-09 23:08:21 +0200

    virtual still not working

--- src/main/java/com/slomaxonical/croptopia/chocolaterie/CroptopiaChocolaterie.java
+++ src/main/java/com/slomaxonical/croptopia/chocolaterie/CroptopiaChocolaterie.java
@@ -2,22 +2,28 @@ package com.slomaxonical.croptopia.chocolaterie;
 
 import com.slomaxonical.croptopia.chocolaterie.registry.ItemRegistry;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
-import static me.thonk.croptopia.Croptopia.CROPTOPIA_ITEM_GROUP;
 
 public class CroptopiaChocolaterie implements ModInitializer {
 
+    public static String MOD_ID = "cacao";
+    public static final ItemGroup COCOA_ITEM_GROUP = FabricItemGroupBuilder.create(new Identifier(MOD_ID, "croptopia"))
+            .icon(() -> new ItemStack(ItemRegistry.MILK_CHOCOLATE))
+            .build();
     public static Identifier createIdentifier(String name) {
-        return new Identifier("cacao", name);
+        return new Identifier(MOD_ID, name);
     }
     public static Item.Settings createGroup() {
-            return new FabricItemSettings().group(CROPTOPIA_ITEM_GROUP);
+            return new FabricItemSettings().group(COCOA_ITEM_GROUP);
         }
     static final String CROPTOPIA_MOD_ID = "croptopia";
 
